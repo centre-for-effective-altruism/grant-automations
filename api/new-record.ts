d5ef9bedfcdd80e5eb0e823d212fb0f44263af52
colophonemes <dev@colophonemes.com>
2021-12-10 15:37:57 +0000

    Test if node_modules error is specific to http-errors

--- api/new-record.ts
+++ api/new-record.ts
@@ -1,11 +1,13 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
-import createError from 'http-errors'
+// import createError from 'http-errors'
+import isNumber from 'is-number'
 
-import { requestWrapper } from '../../lib/helpers'
+import { requestWrapper } from '../lib/helpers'
 
 async function postHandler(req: VercelRequest, res: VercelResponse) {
   const { recordId } = req.body
-  if (!recordId) throw createError(400, 'Invalid record id')
+  console.log(`is 10 a number? ${isNumber(10)}`)
+  if (!recordId) throw new Error('no record Id') // createError(400, 'Invalid record id')
   console.log(`Got record ID ${recordId}`)
   res.send(`Thanks for sending record ID ${recordId}`)
 }
